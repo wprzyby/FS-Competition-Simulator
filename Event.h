0f6918c9ad37b4715cb9a45d963a3ff36d88a440
asieruga <adamsie.protondynamic@gmail.com>
2022-05-08 16:10:05 +0200

    Made Event methods as public

--- Event.h
+++ Event.h
@@ -14,13 +14,12 @@ class Event  // Abstract class
     std::map<Team, std::map<std::string, double>> teams_and_results;  // Map with teams and second map of attributes created from Teams vector by a method.
     std::map<Team, const double> classification;  // Array with teams and total points scored in the Event [Team, TotalPoints].
 
+    public:
     // Methods:
     virtual void create_teams_and_results_map()=0;  // Function to create teams_and_results map.
     void set_teams_results(std::map<Team,std::vector<double>> &results);  // Function to set results from provided vector in the event for every team participating.
     virtual void calculate_teams_points()=0;  // Function that calculates points for teams (based on the map: teams_and_results).
     void make_final_classification();  // Function that sorts teams by their total score (not cirtual, as it only sorts the map by the amount of points that teams scored - same for every competition).
-
-    public:
     // Constructor:
     Event();
     // Getters:
