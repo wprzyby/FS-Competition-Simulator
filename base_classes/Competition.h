b409c6624a6b1434eb070b45c04fa8ebb7270840
Wojciech Przybylski <pwojtek40@gmail.com>
2022-05-15 19:37:05 +0200

    Competition functionality update
    - adjusted competition simulation
    - added final classification attribute
    - implemented final classification creation

--- base_classes/Competition.h
+++ base_classes/Competition.h
@@ -14,31 +14,29 @@
 class Competition
 {
     private:
-    std::vector<std::unique_ptr<Event>> m_events;
-    std::vector<Team> m_teams;
-    // TODO: zobaczyć czy to w ogóle używam
-    std::map<event_type, std::map<Team, double>> m_events_points;
-    std::map<Team, unsigned> m_competition_points;
+        std::vector<std::unique_ptr<Event>> m_events;
+        std::vector<Team> m_teams;
+        std::map<event_type, std::map<Team, double>> m_events_points;
+        std::map<Team, unsigned> m_competition_points;
+        std::vector< std::pair<Team, double> > m_final_classification;
 
     public:
-    Competition();
+        Competition();
 
-    // getters:
-    std::vector<std::unique_ptr<Event>> get_events() const {return m_events;}
-    std::vector<Team> get_teams() const {return m_teams;}
-    std::map<event_type, std::map<Team, double>> get_events_points() const {return m_events_points;}
-    std::map<Team, double> get_competition_results() const;
+        // getters:
+        std::vector<std::unique_ptr<Event>> get_events() const {return m_events;}
+        std::vector<Team> get_teams() const {return m_teams;}
+        std::map<event_type, std::map<Team, double>> get_events_points() const {return m_events_points;}
+        std::vector< std::pair<Team, double> > get_final_classification() const {return m_final_classification;}
 
-    // setters:
-    // (boolean returns to communicate whether the setter succeeded or failed)
-    bool set_events(std::vector<std::unique_ptr<Event>> events);
-    bool set_teams(std::vector<Team> teams);
+        // setters:
+        // (boolean returns to communicate whether the setter succeeded or failed)
+        bool set_events(std::vector<std::unique_ptr<Event>> events);
+        bool set_teams(std::vector<Team> teams);
 
-    // essential competition functionality:
-    void simulate_competition();
-    void create_classification();
-    void show_classification();
-    // classification - wektor par drużyna-wynik, posortowany od najwyższego wyniku do najniższego
+        // essential competition functionality:
+        void simulate();
+        void create_classification();
 };
 
 
