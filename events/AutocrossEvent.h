a50bb0ad5922c9ee20829a471ea08ea9e78d9e85
Wojciech Przybylski <pwojtek40@gmail.com>
2022-11-18 22:20:23 +0100

    Implemented initial version of Autocross DC
    Implementation according to FS Rules 2023 (significant changes to Autocross DC compared to 2022)
    For: #19

--- events/AutocrossEvent.h
+++ events/AutocrossEvent.h
@@ -18,3 +18,21 @@ class AutocrossEvent : public Event  // AutocrossEvent class that inherits from 
       AutocrossEvent() {init_event_type(autocross);}
       AutocrossEvent(std::vector<Team> &teams): Event(teams) {init_event_type(autocross);}
 };
+
+
+class DCAutocrossEvent: public Event {
+    private:
+      double m_t_max;
+
+    protected:
+      std::map<Team, double> find_teams_best_times() override;
+      void fill_teams_points() override;
+
+    public:
+      DCAutocrossEvent(double time_6ms): m_t_max(time_6ms) {init_event_type(autocross_DC);}
+      DCAutocrossEvent(double time_6ms, std::vector<Team>& teams): Event(teams), m_t_max(time_6ms) {init_event_type(autocross_DC);}
+
+    private:
+      double get_additional_points(double best_time_overall, double team_best_time);
+
+};
