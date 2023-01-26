e5a498fc81ea42e89d90ce1199029d1f6bbaf331
Wojciech Przybylski <pwojtek40@gmail.com>
2023-01-26 13:22:15 +0100

    Fixed error with penalty config for DC/DV events
    For : #27

--- laptime_parser/constants.h
+++ laptime_parser/constants.h
@@ -4,10 +4,16 @@
 #include <compsim_enums/enums.h>
 #include <map>
 
-const std::map<EventType, std::string> DYNAMIC_EVENT_ENUN_TO_STR = {{acceleration, "acceleration"},
+// ONLY FOR THE PURPOSE OF READING THE JSON PENALTY CONFIG FILE
+const std::map<EventType, std::string> EVENT_ENUM_TO_STR_PENALTY_CONFIG = {{acceleration, "acceleration"},
                                                                      {autocross, "autocross"},
                                                                      {endurance, "endurance"},
-                                                                     {skidpad, "skidpad"}};
+                                                                     {skidpad, "skidpad"},
+                                                                     {acceleration_DC, "acceleration"},
+                                                                     {autocross_DC, "autocross"},
+                                                                     {skidpad_DC, "skidpad"},
+                                                                     {acceleration_DV, "acceleration"},
+                                                                     {skidpad_DV, "skidpad"}};
 
 
 #endif
